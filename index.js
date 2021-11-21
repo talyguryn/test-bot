b046d6d1bbb4e72f4a4fa3af5112b2ff9fa34e2b
Taly <vitalik7tv@yandex.ru>
2021-11-22 02:53:22 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * Load environment variables
+ */
+require('dotenv').config();
+
+const TelegramBot = require('node-telegram-bot-api');
+
+const BOT_TOKEN = process.env.BOT_TOKEN;
+const bot = new TelegramBot(BOT_TOKEN, {polling: true});
+
+bot.on('polling_error', function(error){ console.log(error); });
+
+console.log(process.env);
+
+bot.onText(/\/start/, (msg, match) => {
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(chatId, 'test-bot 0.0.0');
+});
